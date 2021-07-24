a462e6a8a28bee5d4c497de25312c5fbe0ad9110
dbergi <dberniell@multiestetica.com>
2021-07-24 18:56:13 +0200

    Add migration file

--- Api/src/Infrastructure/Persistence/Doctrine/Repository/TaskRepository.php
+++ Api/src/Infrastructure/Persistence/Doctrine/Repository/TaskRepository.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 namespace App\Infrastructure\Persistence\Doctrine\Repository;
 
 
+use App\Domain\Task\Task;
 use App\Domain\Task\TaskRepositoryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class TaskRepository extends MysqlRepository implements TaskRepositoryInterface
 {
-
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Task::class);
+    }
 }
